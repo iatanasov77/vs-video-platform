b5f0d8561eeacede171d433a1dbea82acb2c8277
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-02-28 07:40:52 +0000

    Add a Helper Field into RefreshToken Entity.

--- src/Entity/Api/RefreshToken.php
+++ src/Entity/Api/RefreshToken.php
@@ -7,4 +7,31 @@ use Vankosoft\ApiBundle\Model\RefreshToken as BaseRefreshToken;
 #[ORM\Table(name: "VSAPI_RefreshTokens")]
 class RefreshToken extends BaseRefreshToken
 {
+    /** @var array */
+    #[ORM\Column(name: "jobs_finished", type: "json", nullable: false)]
+    private $jobsFinished;
+    
+    public function __construct()
+    {
+        $this->jobsFinished = [];
+    }
+    
+    public function getJobsFinished()
+    {
+        return $this->jobData;
+    }
+    
+    public function setJobsFinished($jobsFinished)
+    {
+        $this->jobsFinished  = $jobsFinished;
+        
+        return $this;
+    }
+    
+    public function addJobFinished($jobFinished)
+    {
+        $this->jobsFinished[]  = $jobFinished;
+        
+        return $this;
+    }
 }
