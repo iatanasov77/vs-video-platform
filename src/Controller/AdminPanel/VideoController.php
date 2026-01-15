afadb71d64351da7a0364328bb723d94b6848616
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-01-15 11:07:41 +0000

    Improve Admin Panel Delete Video Action.

--- src/Controller/AdminPanel/VideoController.php
+++ src/Controller/AdminPanel/VideoController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use League\Flysystem\UnableToDeleteFile;
 use Doctrine\Common\Collections\ArrayCollection;
 use Sylius\Component\Resource\ResourceActions;
 use Vankosoft\ApplicationBundle\Component\Status;
@@ -67,7 +68,12 @@ class VideoController extends AbstractCrudController
         
         //$this->removeThumbnailFile( $videoThumbnail, $videoId );
         $this->removeVideoPhotos( $resource );
-        $this->removeVideoFile( $videoFile, $videoId );
+        
+        try {
+            $this->removeVideoFile( $videoFile, $videoId );
+        } catch ( UnableToDeleteFile $e ) {
+            $request->getSession()->getFlashBag()->add( 'error', 'Unable To Delete File From Storage !' );
+        }
         
         if ( $resource->getVideoTrailer() ) {
             $this->removeVideoTrailer( $resource->getVideoTrailer() );
