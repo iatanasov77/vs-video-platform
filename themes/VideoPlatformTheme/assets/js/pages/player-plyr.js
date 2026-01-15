7c14016d3c2760e7304d54bbaab3917ad08ea825
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-01-15 11:03:39 +0000

    Enable Tooltips on Plyr Player Controls.

--- themes/VideoPlatformTheme/assets/js/pages/player-plyr.js
+++ themes/VideoPlatformTheme/assets/js/pages/player-plyr.js
@@ -1,13 +1,44 @@
-import Plyr from 'plyr';
 require( '../../css/plyr.scss' );
+
+//const Plyr = require( '../../vendor/themeforest-flixgo-online-movies/js/plyr.min.js' );
+import Plyr from 'plyr';
 window.Plyr = Plyr;
+
+var watermarkText;
+var currentClass        = 0;
+var watermarkClasses    = [
+    'WatermarkText_BottomRight',
+    'WatermarkText_BottomLeft',
+    'WatermarkText_TopLeft',
+    'WatermarkText_TopRight',
+];
+    
+function moveWatermark()
+{
+    setTimeout( () => {
+        $( '.' + watermarkClasses[currentClass] ).remove();
+        
+        currentClass    = currentClass == 3 ? 0 : currentClass + 1;
+        $( '.plyr--video' ).prepend( '<div class="' + watermarkClasses[currentClass] + '"> ' + watermarkText + ' </div>' );
+        
+        moveWatermark();
+    }, "10000" ); // Delayed for 10 seconds
+}
 
 $( function()
 {
-    const player        = new Plyr( '#player-plyr' );
+    /**
+     * Player Controls Options
+     * ========================
+     * https://github.com/sampotts/plyr/blob/master/CONTROLS.md
+     */
+    const player    = new Plyr( '#player-plyr', {
+        tooltips: { controls: true, seek: true }
+    });
     
     let watermarkText   = $( '#player-plyr' ).attr( 'data-watermarkText' );
     $( '.plyr--video' ).prepend( '<div class="WatermarkText"> ' + watermarkText + ' </div>' );
+    moveWatermark();
     
     player.on( 'play', function()
     {
